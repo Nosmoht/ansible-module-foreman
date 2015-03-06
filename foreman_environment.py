ad2477661db0971c39a813f0731deb5a222d4eef
tkrah <thomas.krahn@esailors.de>
2015-03-06 16:44:05 +0100

    Add module to create and delete environments

---
+++ foreman_environment.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from foreman import Foreman
+
+def ensure(module):
+    changed = False
+    # Set parameters
+    name = module.params['name']
+    state = module.params['state']
+    foreman_host = module.params['foreman_host']
+    foreman_port = module.params['foreman_port']
+    foreman_user = module.params['foreman_user']
+    foreman_pass = module.params['foreman_pass']
+    theforeman = Foreman(hostname=foreman_host, port=foreman_port, username=foreman_user, password=foreman_pass)
+    env = theforeman.get_environment_by_name(name=name)
+    if not env and state == 'present':
+        theforeman.create_environment(name=name)
+        changed = True
+    if env and state == 'absent':
+        theforeman.delete_environment(name=name)
+        changed = True
+    return changed
+
+def main():
+    module = AnsibleModule(
+        argument_spec=dict(
+            name=dict(Type='str', required=True),
+            state=dict(Type='str', Default='present', choices=['present', 'absent']),
+            foreman_host=dict(Type='str', Default='127.0.0.1'),
+            foreman_port=dict(Type='str', Default='443'),
+            foreman_user=dict(Type='str', required=True),
+            foreman_pass=dict(Type='str', required=True)
+        ),
+    )
+
+    changed = ensure(module)
+    module.exit_json(changed=changed, name=module.params['name'])
+
+# import module snippets
+from ansible.module_utils.basic import *
+main()

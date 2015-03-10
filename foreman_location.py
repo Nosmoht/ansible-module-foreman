5f706cf8b8243a2f73d68adcf9f95faabacd94be
tkrah <thomas.krahn@esailors.de>
2015-03-10 08:54:35 +0100

    Add function to manage locations

---
+++ foreman_location.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from foreman import Foreman
+
+def ensure(module):
+    name = module.params['name']
+    state = module.params['state']
+    foreman_host = module.params['foreman_host']
+    foreman_port = module.params['foreman_port']
+    foreman_user = module.params['foreman_user']
+    foreman_pass = module.params['foreman_pass']
+    theforeman = Foreman(hostname=foreman_host,
+                         port=foreman_port,
+                         username=foreman_user,
+                         password=foreman_pass)
+    data = {}
+    data['name'] = name
+    domain = theforeman.get_location(data=data)
+    if not domain and state == 'present':
+        theforeman.create_location(data=data)
+        return True
+    if domain and state == 'absent':
+        theforeman.delete_location(data=domain)
+        return True
+    return False
+
+def main():
+    module = AnsibleModule(
+        argument_spec=dict(
+            name=dict(Type='str', required=True),
+            state=dict(Type='str', Default='present', choices=['present', 'absent']),
+            foreman_host=dict(Type='str', Default='127.0.0.1'),
+            foreman_port=dict(Type='str', Default='443'),
+            foreman_user=dict(Type='str', required=True),
+            foreman_pass=dict(Type='str', required=True)
+        ),
+    )
+
+    changed = ensure(module)
+    module.exit_json(changed=changed, name=module.params['name'])
+
+# import module snippets
+from ansible.module_utils.basic import *
+main()

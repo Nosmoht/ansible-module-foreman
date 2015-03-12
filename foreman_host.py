74ba23f7913090590ac2ef3d8891c6df4f5016f4
tkrah <thomas.krahn@esailors.de>
2015-03-12 14:55:43 +0100

    Set parameters to required=True if they are. Add parameter build to instruct Foreman to build the host. Add parameter root_pass. Set data[] attributes only if defined.

--- foreman_host.py
+++ foreman_host.py
@@ -7,6 +7,7 @@ def ensure(module):
     changed = False
     name = module.params['name']
     architecture = module.params['architecture']
+    build = module.params['build']
     compute_profile = module.params['compute_profile']
     compute_resource = module.params['compute_resource']
     domain = module.params['domain']
@@ -16,6 +17,7 @@ def ensure(module):
     medium = module.params['medium']
     operatingsystem = module.params['operatingsystem']
     organization = module.params['organization']
+    root_pass = module.params['root_pass']
     state = module.params['state']
     foreman_host = module.params['foreman_host']
     foreman_port = module.params['foreman_port']
@@ -33,16 +35,28 @@ def ensure(module):
         if state == 'absent':
             return False
         data['architecture_id'] = theforeman.get_architecture(data={'name': architecture}).get('id')
-        data['compute_profile_id'] = theforeman.get_compute_profile(data={'name': compute_profile}).get('id')
-        data['compute_resource_id'] = theforeman.get_compute_resource(data={'name': compute_resource}).get('id')
-        data['domain_id'] = theforeman.get_domain(data={'name': domain}).get('id')
-        data['environment_id'] = theforeman.get_environment(data={'name': environment}).get('id')
-        data['hostgroup_id'] = theforeman.get_hostgroup(data={'name': hostgroup}).get('id')
+        if build:
+            data['build'] = 'true'
+        else:
+            data['build'] = 'false'
+        if compute_profile:
+            data['compute_profile_id'] = theforeman.get_compute_profile(data={'name': compute_profile}).get('id')
+        if compute_resource:
+            data['compute_resource_id'] = theforeman.get_compute_resource(data={'name': compute_resource}).get('id')
+        if domain:
+            data['domain_id'] = theforeman.get_domain(data={'name': domain}).get('id')
+        if environment:
+            data['environment_id'] = theforeman.get_environment(data={'name': environment}).get('id')
+        if hostgroup:
+            data['hostgroup_id'] = theforeman.get_hostgroup(data={'name': hostgroup}).get('id')
         data['location_id'] = theforeman.get_location(data={'name': location}).get('id')
-        data['medium_id'] = theforeman.get_medium(data={'name' :medium}).get('id')
+        if medium:
+            data['medium_id'] = theforeman.get_medium(data={'name' :medium}).get('id')
         data['organization_id'] = theforeman.get_organization(data={'name': organization}).get('id')
-        data['operatingsystem_id'] = theforeman.get_operatingsystem(data={'name': operatingsystem}).get('id')
-
+        if operatingsystem:
+            data['operatingsystem_id'] = theforeman.get_operatingsystem(data={'name': operatingsystem}).get('id')
+        if root_pass:
+            data['root_pass'] = root_pass
         host = theforeman.create_host(data=data)
         changed = True
 
@@ -67,15 +81,17 @@ def main():
         argument_spec=dict(
             name=dict(Type='str', required=True),
             architecture=dict(Type='str', Default='x86_64'),
+            build=dict(Type='bool', Default=True),
             compute_profile=dict(Type='str'),
             compute_resource=dict(Type='str'),
             domain=dict(Type='str'),
             environment=dict(Type='str'),
             hostgroup=dict(Type='str'),
-            location=dict(Type='str'),
+            location=dict(Type='str', required=True),
             medium=dict(Type='str'),
             operatingsystem=dict(Type='str'),
-            organization=dict(Type='str'),
+            organization=dict(Type='str', required=True),
+            root_pass=dict(Type='str'),
             state=dict(Type='str', Default='present', choices=['present', 'absent', 'running', 'stopped', 'rebooted']),
             foreman_host=dict(Type='str', Default='127.0.0.1'),
             foreman_port=dict(Type='str', Default='443'),

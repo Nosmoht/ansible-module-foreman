e9d7ebf82b7bc1217b7f52b76d19da314c459ca8
tkrah <thomas.krahn@esailors.de>
2015-03-13 09:39:24 +0100

    Add missing module fail for Smart Proxy

--- foreman_hostgroup.py
+++ foreman_hostgroup.py
@@ -35,58 +35,59 @@ def ensure(module):
             if not architecture:
                 module.fail_json(msg='Architecture not found: ' + architecture_name)
             data['architecture_id'] = architecture.get('id')
-            
+
         # Domain
         if domain_name:
             domain = theforeman.get_domain(data={'name': domain_name})
             if not domain:
                 module.fail_json(msg='Domain not found: ' + domain_name)
             data['domain_id'] = domain.get('id')
-            
+
         # Environment
         if environment_name:
             environment = theforeman.get_environment(data={'name': environment_name})
             if not environment:
                 module.fail_json(msg='Environment not found: ' + environment_name)
             data['environment_id'] = environment.get('id')
-            
+
         # Medium
         if medium_name:
             medium = theforeman.get_medium(data={'name':medium_name})
             if not medium:
                 module.fail_json(msg='Medium not found: ' + medium_name)
             data['medium_id'] = medium.get('id')
-            
+
         # Operatingsystem
         if operatingsystem_name:
             operatingsystem = theforeman.get_operatingsystem(data={'name': operatingsystem_name})
             if not operatingsystem:
                 module.fail_json(msg='Operatingsystem not found: ' + operatingsystem_name)
             data['operatingsystem_id'] = operatingsystem.get('id')
-        
+
         # Partition Table
         if partition_table_name:
             partition_table = theforeman.get_partition_table(data={'name': partition_table_name})
             if not partition_table:
                 module.fail_json(msg='Partition Table not found: ' + partition_table_name)
             data['ptable_id'] = partition_table.get('id')
-            
+
         # Smart Proxy
         if smart_proxy_name:
             smart_proxy = theforeman.get_smart_proxy(data={'name':smart_proxy_name})
             if not smart_proxy:
+                module.fail_json(msg='Smart Proxy not found: ' + smart_proxy_name)
             data['puppet_proxy_id'] = smart_proxy.get('id')
-            
+
         # Subnet
         if subnet_name:
             subnet = theforeman.get_subnet(data={'name':subnet_name})
             if not subnet:
                 module.fail_json(msg='Subnet not found: ' + subnet_name)
             data['subnet_id'] = subnet.get('id')
-            
+
         theforeman.create_hostgroup(data=data)
         changed = True
-    
+
     if hostgroup and state == 'absent':
         theforeman.delete_hostgroup(data=hostgroup)
         changed = True

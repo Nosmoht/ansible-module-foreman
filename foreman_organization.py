aafcea14c26553222f3738dad91f098533bffcf2
tkrah <thomas.krahn@esailors.de>
2015-04-16 13:56:21 +0200

    Add foreman_organization to be able to configure them

---
+++ foreman_organization.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from foreman.foreman import *
+except ImportError:
+    foremanclient_found = False
+else:
+    foremanclient_found = True
+
+def ensure(module):
+    name = module.params['name']
+    state = module.params['state']
+    foreman_host = module.params['foreman_host']
+    foreman_port = module.params['foreman_port']
+    foreman_user = module.params['foreman_user']
+    foreman_pass = module.params['foreman_pass']
+    theforeman = Foreman(hostname=foreman_host,
+                         port=foreman_port,
+                         username=foreman_user,
+                         password=foreman_pass)
+    data = {}
+    data['name'] = name
+
+    try:
+        organization = theforeman.search_organization(data=data)
+    except ForemanError as e:
+        module.fail_json(msg='Could not get organization: {0}'.format(e.message))
+
+    if not organization and state == 'present':
+        try:
+            theforeman.create_organization(data=data)
+            return True
+        except ForemanError as e:
+            module.fail_json(msg='Could not create organization: {0}'.format(e.message))
+
+    if organization and state == 'absent':
+        try:
+            theforeman.delete_organization(id=organization.get('id'))
+            return True
+        except ForemanError as e:
+            module.fail_json('Could not delete organization: {0}'.format(e.message))
+
+    return False
+
+def main():
+    module = AnsibleModule(
+        argument_spec=dict(
+            name=dict(Type='str', required=True),
+            state=dict(Type='str', Default='present', choices=['present', 'absent']),
+            foreman_host=dict(Type='str', Default='127.0.0.1'),
+            foreman_port=dict(Type='str', Default='443'),
+            foreman_user=dict(Type='str', required=True),
+            foreman_pass=dict(Type='str', required=True)
+        ),
+    )
+
+    if not foremanclient_found:
+        module.fail_json(msg='python-foreman module is required. See https://github.com/Nosmoht/python-foreman.')
+
+    changed = ensure(module)
+    module.exit_json(changed=changed, name=module.params['name'])
+
+# import module snippets
+from ansible.module_utils.basic import *
+main()

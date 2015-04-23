eb73332ceddd048430bbae56bc6b83f74b4e23ea
tkrah <thomas.krahn@esailors.de>
2015-04-23 22:56:17 +0200

    Add Ansible documentation

--- foreman_operatingsystem.py
+++ foreman_operatingsystem.py
@@ -1,6 +1,67 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+DOCUMENTATION = '''
+---
+module: foreman_location
+short_description:
+- Manage Foreman Operatingsystems using Foreman API v2.
+description:
+- Create, update and and delete Foreman Operatingsystems using Foreman API v2
+options:
+  name:
+    description: OS name
+    required: True
+    default: null
+    aliases: []
+  state:
+    description: OS state
+    required: False
+    default: 'present'
+    choices: ['present', 'absent']
+  major:
+    description: OS major version
+    required: true
+    default: null
+  minor:
+    description: OS minor version
+    required: false
+    default: null
+  foreman_host:
+    description: Hostname or IP address of Foreman system
+    required: false
+    default: 127.0.0.1
+  foreman_port:
+    description: Port of Foreman API
+    required: false
+    default: 443
+  foreman_user:
+    description: Username to be used to authenticate on Foreman
+    required: true
+    default: null
+  foreman_pass:
+    description: Password to be used to authenticate user on Foreman
+    required: true
+    default: null
+notes:
+- Requires the python-foreman package to be installed. See https://github.com/Nosmoht/python-foreman.
+author: Thomas Krahn
+'''
+
+EXAMPLES = '''
+- name: Ensure CoreOS 607.0.0
+  foreman_operatingsystem:
+    name: CoreOS 607.0.0
+    description: CoreOS Current stable
+    major: 607
+    minor: 0.0
+    state: present
+    foreman_host: 127.0.0.1
+    foreman_port: 443
+    foreman_user: admin
+    foreman_pass: secret
+'''
+
 try:
     from foreman.foreman import *
 

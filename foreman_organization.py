3eb78cd3f9d26d642a73f3dc3724cbc8433a772a
tkrah <thomas.krahn@esailors.de>
2015-10-14 19:27:51 +0200

    Add Ansible module description of foreman_organization module

--- foreman_organization.py
+++ foreman_organization.py
@@ -16,6 +16,41 @@
 # You should have received a copy of the GNU General Public License
 # along with this software.  If not, see <http://www.gnu.org/licenses/>.
 
+DOCUMENTATION = '''
+---
+module: foreman_organization
+short_description: Manage Foreman organization resources using Foreman API v2
+description:
+- Create and delete Foreman organization resources using Foreman API v2
+options:
+  name:
+    description: Organization name
+    required: true
+  state:
+    description: Organization state
+    required: false
+    default: present
+    choices: ["present", "absent"]
+  foreman_host:
+    description: Hostname or IP address of Foreman system
+    required: false
+    default: 127.0.0.1
+  foreman_port:
+    description: Port of Foreman API
+    required: false
+    default: 443
+  foreman_user:
+    description: Username to be used to authenticate on Foreman
+    required: true
+  foreman_pass:
+    description: Password to be used to authenticate user on Foreman
+    required: true
+notes:
+- Requires the python-foreman package to be installed. See https://github.com/Nosmoht/python-foreman.
+version_added: "2.0"
+author: Thomas Krahn (@Nosmoht)
+'''
+
 try:
     from foreman.foreman import *
 except ImportError:

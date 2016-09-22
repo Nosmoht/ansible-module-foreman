Nosmoht/ansible-module-foreman

commit a61f59e4fb7ab7a1c5f2223ea482e9ee25eba9e0

Add boot_mode argument

foreman_subnet.py
@@ -55,6 +55,11 @@ options:
     required: False
     default: None
     choices: ['DHCP', 'Internal DB', 'None']),
+  boot_mode:
+    description: Default boot mode for interfaces assigned to this subnet
+    required: False
+    default: None
+    choices: ['DHCP', 'Static']),
   ip_from:
     description: Starting IP Address for IP auto suggestion
     required: False
@@ -115,6 +120,7 @@ EXAMPLES = '''
     domains:
       - foo.example.com
     ipam: DHCP
+    boot_mode: Static
     ip_from: 192.168.123.3
     ip_to: 192.168.123.253
     gateway: 192.168.123.254
@@ -180,7 +186,7 @@ def ensure(module):
     except ForemanError as e:
         module.fail_json(msg='Could not get subnet: {0}'.format(e.message))
 
-    for key in ['dns_primary', 'dns_secondary', 'gateway', 'ipam', 'mask', 'network', 'network_address', 'vlanid']:
+    for key in ['dns_primary', 'dns_secondary', 'gateway', 'ipam', 'boot_mode', 'mask', 'network', 'network_address', 'vlanid']:
         if key in module.params:
             data[key] = module.params[key]
     if 'ip_from' in module.params:
@@ -235,6 +241,7 @@ def main():
             network=dict(type='str', required=False),
             mask=dict(type='str', required=False),
             ipam=dict(type='str', required=False, choices=['DHCP', 'Internal DB', 'None']),
+            boot_mode=dict(type='str', required=False, choices=['DHCP', 'Static']),
             ip_from=dict(type='str', required=False),
             ip_to=dict(type='str', required=False),
             state=dict(type='str', default='present', choices=['present', 'absent']),

commit d4c8927aae766e11bcc324273024a79c6154ebca

ansible-foreman-module: set default boot mode
Change a61f59e4fb7ab7a1c5f2223ea482e9ee25eba9e0 broke subnets without a
boot_mode like:

  "Could not update subnet: Boot mode is not included in the list"

foreman_subnet.py
@@ -58,7 +58,7 @@ options:
   boot_mode:
     description: Default boot mode for interfaces assigned to this subnet
     required: False
-    default: None
+    default: 'DHCP'
     choices: ['DHCP', 'Static']),
   ip_from:
     description: Starting IP Address for IP auto suggestion
@@ -249,7 +249,7 @@ def main():
             network=dict(type='str', required=False),
             mask=dict(type='str', required=False),
             ipam=dict(type='str', required=False, choices=['DHCP', 'Internal DB', 'None']),
-            boot_mode=dict(type='str', required=False, choices=['DHCP', 'Static']),
+            boot_mode=dict(type='str', required=False, choices=['DHCP', 'Static'], default='DHCP'),
             ip_from=dict(type='str', required=False),
             ip_to=dict(type='str', required=False),
             state=dict(type='str', default='present', choices=['present', 'absent']),
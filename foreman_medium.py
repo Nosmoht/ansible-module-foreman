0c4ff83018f256d24a959e28032dd01dc4313720
bernhardh <bernhardh@atix.de>
2017-04-13 11:14:41 +0200

    add functionality to assign locations and organizations to media

--- foreman_medium.py
+++ foreman_medium.py
@@ -25,7 +25,7 @@ description:
 options:
   name:
     description:
-    - Medium name
+    - Medium name, a combination of name '*' and state 'absent' can be used to clean up, by deleting all present media
     required: true
   path:
     description:
@@ -37,10 +37,18 @@ options:
     required: false
   state:
     description:
-    - Medium state
+    - Medium state, , a combination of name '*' and state 'absent' can be used to clean up, by deleting all present media
     required: false
     default: 'present'
     choices: ['present', 'absent']
+  organizations:
+    description:
+    - List of organization the medium should be assigned to
+    required: false
+  locations:
+    description:
+    - List of locations the medium should be assigned to
+    required: false
   foreman_host:
     description:
     - Hostname or IP address of Foreman system
@@ -75,6 +83,11 @@ EXAMPLES = '''
     name: CentOS mirror
     path: http://mirror.centos.org/centos/$version/os/$arch
     os_family: RedHat
+    organizations:
+    - Development
+    - DevOps
+    locations:
+    - Munich
     state: present
     foreman_user: admin
     foreman_pass: secret
@@ -90,11 +103,40 @@ except ImportError:
     foremanclient_found = False
 
 
+def get_organization_ids(module, theforeman, organizations):
+    result = []
+    for i in range(0, len(organizations)):
+        try:
+            organization = theforeman.search_organization(data={'name': organizations[i]})
+            if not organization:
+                module.fail_json('Could not find Organization {0}'.format(organizations[i]))
+            result.append(organization.get('id'))
+        except ForemanError as e:
+            module.fail_json('Could not get Organizations: {0}'.format(e.message))
+    return result
+
+
+def get_location_ids(module, theforeman, locations):
+    result = []
+    for i in range(0, len(locations)):
+        try:
+            location = theforeman.search_location(data={'name':locations[i]})
+            if not location:
+                module.fail_json('Could not find Location {0}'.format(locations[i]))
+            result.append(location.get('id'))
+        except ForemanError as e:
+            module.fail_json('Could not get Locations: {0}'.format(e.message))
+    return result
+
+
 def ensure(module):
     name = module.params['name']
     path = module.params['path']
     state = module.params['state']
     os_family = module.params['os_family']
+
+    organizations = module.params['organizations']
+    locations = module.params['locations']
 
     foreman_host = module.params['foreman_host']
     foreman_port = module.params['foreman_port']
@@ -110,6 +152,16 @@ def ensure(module):
 
     data = {'name': name}
 
+
+    if name == '*' and state == 'absent':
+        try:
+            all_media_list = theforeman.get_resources(resource_type=MEDIA)
+            for element in all_media_list:
+                theforeman.delete_medium(id=element.get('id'))
+            return True, all_media_list
+        except ForemanError as e:
+            module.fail_json(msg='Error in deleting all existing media: {0}'.format(e.message))
+
     try:
         medium = theforeman.search_medium(data=data)
     except ForemanError as e:
@@ -117,6 +169,14 @@ def ensure(module):
 
     data['path'] = path
     data['os_family'] = os_family
+
+    if organizations:
+         data['organization_ids'] = get_organization_ids(module, theforeman, organizations)
+
+    if locations:
+         data['location_ids'] = get_location_ids(module, theforeman, locations)
+
+
 
     if not medium and state == 'present':
         try:
@@ -142,12 +202,15 @@ def ensure(module):
     return False, medium
 
 
+
 def main():
     module = AnsibleModule(
         argument_spec=dict(
             name=dict(type='str', required=True),
             path=dict(type='str', required=False),
             os_family=dict(type='str', required=False),
+            organizations=dict(type='list', required=False),
+            locations=dict(type='list', required=False),
             state=dict(type='str', default='present', choices=['present', 'absent']),
             foreman_host=dict(type='str', default='127.0.0.1'),
             foreman_port=dict(type='str', default='443'),
